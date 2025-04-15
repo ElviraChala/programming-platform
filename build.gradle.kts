0ae848e9eb77ee18e42a3b1801082e6d5884be81
elvira <elvira.chalaya0018@gmail.com>
2025-04-15 11:34:55 +0300

    fix tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.4.4")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 	implementation("org.postgresql:postgresql:42.7.5")
-	testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.0-M1")
+
+//	testImplementation("org.junit.jupiter:junit-jupiter-api:5.13.0-M1")
+//	testImplementation("org.junit.jupiter:junit-jupiter-engine:5.13.0-M2")
+//	testImplementation("org.mockito:mockito-core:5.17.0")
+	testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.4")
 }
 
 tasks.withType<Test> {
---
+++ src/test/java/com/elvira/programming_platform/ProgrammingPlatformApplicationTest.java
@@ -0,0 +1,14 @@
+package com.elvira.programming_platform;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgrammingPlatformApplicationTest {
+
+    @Test
+    void testFirst(){
+        assertTrue(true);
+    }
+
+}
