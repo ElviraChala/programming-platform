9c0cfff850a8692cf48f204c39816adb6e5ff0cc
elvira <elvira.chalaya0018@gmail.com>
2025-04-18 09:10:38 +0300

    add QuestionConverter

--- src/main/java/com/elvira/programming_platform/coverter/QuestionConverter.java
+++ src/main/java/com/elvira/programming_platform/coverter/QuestionConverter.java
@@ -2,19 +2,48 @@ package com.elvira.programming_platform.coverter;
 
 import com.elvira.programming_platform.dto.QuestionDTO;
 import com.elvira.programming_platform.model.Question;
+import com.elvira.programming_platform.repository.CheckKnowledgeRepository;
 import org.springframework.stereotype.Component;
 
 @Component
 public class QuestionConverter {
 
-    public Question toModel(QuestionDTO source){
+    private final CheckKnowledgeRepository checkKnowledgeRepository;
+
+    public QuestionConverter(CheckKnowledgeRepository checkKnowledgeRepository) {
+        this.checkKnowledgeRepository = checkKnowledgeRepository;
+    }
+
+    public Question toModel(QuestionDTO source) {
         Question target = new Question();
+        target.setId(source.getId());
+        target.setText(source.getText());
+
+        if (source.getOptions() != null) {
+            target.setOptions(source.getOptions());
+        }
+
+        target.setCorrectAnswer(source.getCorrectAnswer());
+        target.setCheckKnowledge(checkKnowledgeRepository.findById(source.getCheckKnowledgeId())
+                .orElse(null));
 
         return target;
     }
 
-    public QuestionDTO toDTO(Question source){
+    public QuestionDTO toDTO(Question source) {
         QuestionDTO target = new QuestionDTO();
+        target.setId(source.getId());
+        target.setText(source.getText());
+
+        if (source.getOptions() != null) {
+            target.setOptions(source.getOptions());
+        }
+
+        target.setCorrectAnswer(source.getCorrectAnswer());
+        if (source.getCheckKnowledge() != null) {
+
+            target.setCheckKnowledgeId(source.getCheckKnowledge().getId());
+        }
 
         return target;
     }
