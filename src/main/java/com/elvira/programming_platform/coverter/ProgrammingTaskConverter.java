e6abba098870c3ef697e695664a2f566fbbf91ed
elvira <elvira.chalaya0018@gmail.com>
2025-04-18 10:44:38 +0300

    add ProgrammingTaskConverter

---
+++ src/main/java/com/elvira/programming_platform/coverter/ProgrammingTaskConverter.java
@@ -0,0 +1,38 @@
+package com.elvira.programming_platform.coverter;
+
+import com.elvira.programming_platform.dto.ProgrammingTaskDTO;
+import com.elvira.programming_platform.model.ProgrammingTask;
+import com.elvira.programming_platform.repository.LessonRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProgrammingTaskConverter {
+
+    private final LessonRepository lessonRepository;
+
+    public ProgrammingTaskConverter(LessonRepository lessonRepository) {
+        this.lessonRepository = lessonRepository;
+    }
+
+    public ProgrammingTask toModel(ProgrammingTaskDTO source) {
+        ProgrammingTask target = new ProgrammingTask();
+        target.setId(source.getId());
+        target.setTitle(source.getTitle());
+        target.setDescription(source.getDescription());
+        target.setStarterCode(source.getStarterCode());
+        target.setExpectedOutput(source.getExpectedOutput());
+        target.setLesson(lessonRepository.findById(source.getLessonId()).orElse(null));
+        return target;
+    }
+
+    public ProgrammingTaskDTO toDTO(ProgrammingTask source) {
+        ProgrammingTaskDTO target = new ProgrammingTaskDTO();
+        target.setId(source.getId());
+        target.setTitle(source.getTitle());
+        target.setDescription(source.getDescription());
+        target.setStarterCode(source.getStarterCode());
+        target.setExpectedOutput(source.getExpectedOutput());
+        target.setLessonId(source.getLesson().getId());
+        return target;
+    }
+}
