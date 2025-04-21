164a2668c74f302fc3bda9de88916ecbfe82cfaa
elvira <elvira.chalaya0018@gmail.com>
2025-04-21 16:15:49 +0300

    add TheoryService and TheoryController

---
+++ src/main/java/com/elvira/programming_platform/controller/TheoryController.java
@@ -0,0 +1,47 @@
+package com.elvira.programming_platform.controller;
+
+import com.elvira.programming_platform.dto.TheoryDTO;
+import com.elvira.programming_platform.service.TheoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("theory")
+public class TheoryController {
+    private final TheoryService theoryService;
+
+    public TheoryController(TheoryService theoryService) {
+        this.theoryService = theoryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<TheoryDTO> createTheory(@RequestBody TheoryDTO theoryDTO) {
+        return ResponseEntity.ok(theoryService.createTheory(theoryDTO));
+    }
+
+    @GetMapping
+    public ResponseEntity<TheoryDTO> readTheoryById(@RequestParam Long id) {
+        return ResponseEntity.ok(theoryService.readTheoryById(id));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<TheoryDTO>> readAllTheories() {
+        return ResponseEntity.ok(theoryService.readAllTheories());
+    }
+
+    @PutMapping
+    public ResponseEntity<TheoryDTO> updateTheory(@RequestBody TheoryDTO theoryDTO) {
+        TheoryDTO updated = theoryService.updateTheory(theoryDTO);
+        return updated != null ? ResponseEntity.ok(updated) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteTheory(@RequestParam Long id) {
+        theoryService.deleteTheory(id);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+}
---
+++ src/main/java/com/elvira/programming_platform/service/TheoryService.java
@@ -0,0 +1,59 @@
+package com.elvira.programming_platform.service;
+
+import com.elvira.programming_platform.coverter.TheoryConverter;
+import com.elvira.programming_platform.dto.TheoryDTO;
+import com.elvira.programming_platform.model.Theory;
+import com.elvira.programming_platform.repository.TheoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TheoryService {
+    private final TheoryConverter theoryConverter;
+    private final TheoryRepository theoryRepository;
+
+    public TheoryService(TheoryConverter theoryConverter, TheoryRepository theoryRepository) {
+        this.theoryConverter = theoryConverter;
+        this.theoryRepository = theoryRepository;
+    }
+
+    public TheoryDTO createTheory(TheoryDTO theoryDTO) {
+        Theory theoryModel = theoryConverter.toModel(theoryDTO);
+        Theory savedTheory = theoryRepository.save(theoryModel);
+        return theoryConverter.toDTO(savedTheory);
+    }
+
+    public TheoryDTO readTheoryById(Long userId) {
+        Theory findTheory = theoryRepository.findById(userId).orElse(null);
+        if (findTheory == null) {
+            return null;
+        }
+        return theoryConverter.toDTO(findTheory);
+    }
+
+    public List<TheoryDTO> readAllTheories() {
+        List<Theory> findTheories = (List<Theory>) theoryRepository.findAll();
+        return findTheories.stream()
+                .map(theoryConverter::toDTO)
+                .toList();
+    }
+
+    public TheoryDTO updateTheory(TheoryDTO newTheoryDTO) {
+        Long userId = newTheoryDTO.getId();
+        Theory findTheory = theoryRepository.findById(userId).orElse(null);
+        if (findTheory == null) {
+            return null;
+        }
+
+        Theory theoryModel = theoryConverter.toModel(newTheoryDTO);
+        Theory updatedTheory = theoryRepository.save(theoryModel);
+        return theoryConverter.toDTO(updatedTheory);
+    }
+
+    public void deleteTheory(Long id) {
+        if (theoryRepository.existsById(id)) {
+            theoryRepository.deleteById(id);
+        }
+    }
+}
