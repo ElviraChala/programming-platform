86cd9e948632d86005cfcb2bbd70f12e93d948a2
elvira <elvira.chalaya0018@gmail.com>
2025-05-12 05:26:38 +0300

    modify CORS for a background

--- src/main/java/com/elvira/programming_platform/config/CorsConfig.java
+++ src/main/java/com/elvira/programming_platform/config/CorsConfig.java
@@ -14,9 +14,10 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:4200", "http://localhost:8080")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
             }
         };
     }
